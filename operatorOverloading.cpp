4c1c910399ed87691e6511cf7f0665a4ffa729a8
Ali Hassan <108623069+alihassan69@users.noreply.github.com>
2022-07-31 22:28:19 +0500

    Update operatorOverloading.cpp

--- operatorOverloading.cpp
+++ operatorOverloading.cpp
@@ -23,11 +23,25 @@ public:
     {
         var2 = v2;
     }
-    test operator+(test &t3)
+    test operator+(test &t1)
     {
         test result;
-        result.set_var1(var1 + t3.var1);
-        result.set_var2(var2 + t3.var2);
+        result.set_var1(var1 + t1.var1);
+        result.set_var2(var2 + t1.var2);
+        return result;
+    }
+     test operator-(test &t2)
+    {
+        test result;
+        result.set_var1(var1 - t2.var1);
+        result.set_var2(var2 - t2.var2);
+        return result;
+    }
+     test operator/(test &t3)
+    {
+        test result;
+        result.set_var1(var1 / t3.var1);
+        result.set_var2(var2 / t3.var2);
         return result;
     }
     test operator*(test &t4)
@@ -47,13 +61,18 @@ int main()
 {
     test t1(2, 5);
     test t2(9, 7);
-    test t3 = t1 + t2;
-
+    test t11= t1 + t2;
+    test t12 = t1 - t2;
+    test t13 = t1 / t2;
+    test t14 = t1 * t2;
     cout << "Addition" << endl;
-    t3.display();
-    test t4 = t1 * t2;
+    t11.display();
+    cout << "Subtraction" << endl;
+    t12.display();
+    cout << "Division" << endl;
+    t13.display();
     cout << "Multiplication" << endl;
-    t4.display();
+    t14.display();
 
     return 0;
 }
